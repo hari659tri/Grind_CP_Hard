ad9909812cfe68a70c27afb56adeaea59cf4ee97
harikesh tripathi <harikesht845@gmail.com>
2025-08-20 18:38:02 +0530

    Time: 37 ms (45.27%), Space: 40.2 MB (64.33%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,110 @@
+class DisjointSet{
+
+ vector<int>rank,parent;
+ public:
+ DisjointSet(int n){
+    rank.resize(n+1,0);
+    parent.resize(n+1);
+    for(int i=0;i<=n;i++){
+        parent[i]=i;
+    }
+   
+ }
+  
+  // sort(arr.b,arr.e,cmp)-->greatger<int>
+  // static 
+ int findUPar(int node){
+    if(node==parent[node]){
+        return node;
+    }
+    return parent[node]=findUPar(parent[node]);
+   }
+
+   void unionByRank(int u,int v){
+    int ulp_u=findUPar(u);
+    int ulp_v=findUPar(v);
+    if(ulp_u==ulp_v) return;
+    if(rank[ulp_u]<rank[ulp_v]){
+     parent[ulp_u]=ulp_v;
+    }
+    else if(rank[ulp_v]<rank[ulp_u]){
+        parent[ulp_v]=ulp_u;
+    }
+    else{
+        parent[ulp_v]=ulp_u;
+        rank[ulp_u]++;
+    }
+   }
+
+
+};
+
+
+
+class Solution {
+public:
+   vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+
+    int n=accounts.size();
+
+     DisjointSet ds(n);
+
+     unordered_map<string,int>mp;
+
+         
+        for(int i=0;i<n;i++){
+            for(int j=1;j<accounts[i].size();j++){
+             
+              if(mp.find(accounts[i][j])==mp.end()){
+            
+               mp[accounts[i][j]]=i;
+              }
+              else{
+                ds.unionByRank(i,mp[accounts[i][j]]);
+              }
+            }
+        }
+   
+
+
+   vector<vector<string>> mergedMail(n);
+
+
+     for(auto it:mp){
+
+        int node=ds.findUPar(it.second);
+        string mail=it.first;
+
+        mergedMail[node].push_back(mail);
+     }
+
+
+    vector<vector<string>>ans;
+
+
+     for(int i=0;i<n;i++){
+        if(mergedMail[i].size()==0) continue;
+
+        sort(mergedMail[i].begin(),mergedMail[i].end());
+
+        vector<string>temp;
+
+        temp.push_back(accounts[i][0]);
+        
+         for(auto it:mergedMail[i]){
+            temp.push_back(it);
+         }
+     
+        ans.push_back(temp);
+
+     }
+
+
+     return ans;
+
+     
+
+
+
+}
+};
