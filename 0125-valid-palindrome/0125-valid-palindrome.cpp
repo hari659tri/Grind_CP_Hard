2a0004b49726de4529e3505e9801d7776d4dd70a
harikesh tripathi <harikesht845@gmail.com>
2025-09-01 14:04:40 +0530

    Time: 3 ms (31.53%), Space: 9.8 MB (79.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -9,6 +9,26 @@ public:
 
 
   }
+
+
+    bool check(int i,int j,string &s){
+        
+         if(i>=j) return true;
+         
+        if(!isalnum(s[i])){
+           return check(i+1,j,s);
+        }
+        if(!isalnum(s[j])){
+            return check(i,j-1,s);
+        }
+
+      if(tolower(s[i])!=tolower(s[j])) return false;
+     
+        
+      return  check(i+1,j-1,s);
+
+     
+    }
     bool isPalindrome(string s) {
         
         // i can solve it recursion first right it's iterative code
@@ -17,28 +37,32 @@ public:
         int i=0;
         int j=n-1;
 
+        // recursive implementation 
 
-         while(i<j){
+       return check(i,j,s);
+
+
+        //  while(i<j){
            
-            if(!alpha(s[i])){
-             i++;
-              continue;
-            } 
+        //     if(!alpha(s[i])){
+        //      i++;
+        //       continue;
+        //     } 
 
-             if(!alpha(s[j])){
-             j--;
-              continue;
-            } 
-
-
-             if(tolower(s[i])!=tolower(s[j])){
-                return false;
-             }
-             i++;
-             j--;
-         }
+        //      if(!alpha(s[j])){
+        //      j--;
+        //       continue;
+        //     } 
 
 
-         return true;
+        //      if(tolower(s[i])!=tolower(s[j])){
+        //         return false;
+        //      }
+        //      i++;
+        //      j--;
+        //  }
+
+
+        //  return true;
     }
 };
