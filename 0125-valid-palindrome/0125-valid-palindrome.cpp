ddeeac5c0e90bba076abce71b2badad4a9a20e3c
harikesh tripathi <harikesht845@gmail.com>
2025-09-01 13:47:22 +0530

    Time: 0 ms (100%), Space: 10.1 MB (36.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,53 +1,44 @@
 class Solution {
-public:
-  
-  bool isAlpha(char ch){
+public: 
 
-    if((ch>='0'&&ch<='9')||(ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z')){
-        return true;
-    }
+  bool alpha(char ch1){
+    if(('0'<=ch1&&ch1<='9')||('a'<=ch1&&ch1<='z')||('A'<=ch1&&ch1<='Z')) return true;
+    // if(('0'<=ch2&&ch2<='9')||('a'<=ch2&&ch2<='z')) return true;
 
     return false;
-    
+
+
   }
     bool isPalindrome(string s) {
-         
-        int n=s.length();
-    
+        
+        // i can solve it recursion first right it's iterative code
 
+        int n=s.size();
         int i=0;
         int j=n-1;
 
-        while(i<j){
 
-      if(!isAlpha(s[i])){
-        i++;
-        continue;
+         while(i<j){
+           
+            if(!alpha(s[i])){
+             i++;
+              continue;
+            } 
 
-      }
-       
+             if(!alpha(s[j])){
+             j--;
+              continue;
+            } 
 
-       if(!isAlpha(s[j])){
-        j--;
-        continue;
-       }
 
-    
-    if(tolower(s[i])!=tolower(s[j])){
-        return false;
+             if(tolower(s[i])!=tolower(s[j])){
+                return false;
+             }
+             i++;
+             j--;
+         }
+
+
+         return true;
     }
-
-   i++;
-   j--;
-
-
-
-
-        }
-
-
-        return true;
-    }
-
-
 };
