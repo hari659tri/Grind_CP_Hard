8a796f2a1905f28ec464c267a61cad99662d512e
harikesh tripathi <harikesht845@gmail.com>
2025-09-05 10:34:03 +0530

    Time: 7 ms (5.13%), Space: 18 MB (5.39%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+    
+     string ans;
+     int i=0;
+    while(true){
+        char ch =0;
+   
+       for(auto str:strs){
+        if(i>=str.size()){
+            ch=0;
+            break;
+        }
+        if(ch==0){
+            ch=str[i];
+        }
+        else if(str[i]!=ch){
+          ch=0;
+          break;
+        }
+       }
+       if(ch==0){
+        break;
+       }
+    
+       ans.push_back(ch);
+       i++;
+
+     }
+
+     return ans;
+
+       }
+};
