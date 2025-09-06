4bfc38ed93a85d8141e0991cc20a6963177d5068
harikesh tripathi <harikesht845@gmail.com>
2025-09-06 16:11:59 +0530

    Time: 115 ms (35.86%), Space: 99.8 MB (21.11%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,28 +1,64 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long ans=0;
-        long long sum=0;
-        unordered_set<int>st;
+        // brute force....
+    //     int n=nums.size();
+    //     int maxi=0;
+         
+    //     for(int i=0;i<=n-k;i++){
+    //         unordered_set<int>st;
+    //      int sum=0;
+            
+    //         for(int j=i;j<k+i;j++){
+    //             if(st.find(nums[j])!=st.end()){
+    //                 break;
+    //             }
+
+    //          st.insert(nums[j]);
+    //          sum+=nums[j];
+              
+                 
+    //         }
+    //         if(st.size()==k){
+    //             maxi=max(maxi,sum);
+    //         }
+
+           
+    //     }
+
+    //    return maxi;
+    // time complexity-->O(n*k)
+    // space complexity O(k)
 
 
-        int left=0;
-        for(int right=0;right<nums.size();right++){
 
-            while(st.find(nums[right])!=st.end()||st.size()==k){
-                st.erase(nums[left]);
-                sum-=nums[left];
-                left++;
+    // optimized code 
+    int n=nums.size();
+    unordered_set<int>st;
+    long long maxi=0;
+    long long  sum=0;
+    int j=0; // starting pointer 
 
-            }
-            sum+=nums[right];
-            st.insert(nums[right]);
+     for(int i=0;i<n;i++){
+      
+      while(j<n&&st.find(nums[i])!=st.end()||st.size()==k){
+        sum-=nums[j];
+        st.erase(nums[j]);
+        j++;
 
-            if(st.size()==k){
-             ans=max(ans,sum);
-            }
+      }
 
-        }
-        return ans;
+      sum+=nums[i];
+      st.insert(nums[i]);
+      if(st.size()==k){
+        maxi=max(maxi,sum);
+      }
+   
+      
+     }
+      
+
+       return maxi;
+
     }
 };
