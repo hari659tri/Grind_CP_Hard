66bc6df2baa2420f453516cd73544ca6ecf15587
harikesh tripathi <harikesht845@gmail.com>
2025-09-10 19:29:32 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -29,18 +29,46 @@ public:
     //   }
 
       //return false;
-      bool ans=false;
-      for(int i=0;i<m;i++){
+    //   bool ans=false;
+    //   for(int i=0;i<m;i++){
 
-        if(matrix[i][0]<=target&&target<=matrix[i][n-1]){
-           ans= bs(matrix[i],target);
-        }
-      }
+    //     if(matrix[i][0]<=target&&target<=matrix[i][n-1]){
+    //        ans= bs(matrix[i],target);
+    //     }
+    //   }
 
      
        //if(ans) return true;
      
-     return ans==true?true: false;
+    // return ans==true?true: false;
+
+     // time complexity O(mlogn) where m=no of row 
+     // n=no of coloumn...
+     //space complexity O(1)
+
+     // optimized approach....
+
+     // flatten a 2-D matrix into an a 1-D matrix in mind(Brain) not in really
+
+      // now 
+      int low=0;
+      int high=m*n-1;
+
+      while(low<=high){
+        int mid=low+(high-low)/2;
+       
+       int row=mid/n;
+       int col=mid%n;
+
+       if(matrix[row][col]==target) return true;
+       else if(matrix[row][col]<target) low=mid+1;
+       else  high=mid-1;
+
+      }
+
+      return false;
+      
+
       
     }
 };
