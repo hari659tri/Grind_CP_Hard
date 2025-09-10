ee424ddc48ab1d53b9cc7f7954a4fc9194801079
harikesh tripathi <harikesht845@gmail.com>
2025-09-10 19:11:48 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.64%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
+   
+   bool bs(vector<int>&v,int target){
+    int i=0;
+    int j=v.size()-1;
+
+    while(i<=j){
+        int mid=i+(j-i)/2;
+
+        if(v[mid]==target) return true;
+
+        else if(v[mid]<target) i=mid+1;
+        else j=mid-1;
+        
+    }
+    return false;
+   }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
         int m=matrix.size();
         int n=matrix[0].size();
 
       
+    //   for(int i=0;i<m;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(matrix[i][j]==target) return true;
+    //     }
+    //   }
+
+      //return false;
+      bool ans=false;
       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(matrix[i][j]==target) return true;
+
+        if(matrix[i][0]<=target&&target<=matrix[i][n-1]){
+           return bs(matrix[i],target);
         }
       }
 
-      return false;
+     
+       //if(ans) return true;
+     
+     return false;
+      //time complexity O(m*n)
+      //space complexity O(1)
     }
 };
