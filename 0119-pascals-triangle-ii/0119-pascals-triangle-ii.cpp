02b45a1463063e6f59be8391c134dcd3e41777f3
harikesh tripathi <harikesht845@gmail.com>
2025-09-11 12:43:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (94.72%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -14,13 +14,36 @@ public:
         if(rowIndex==0) return {1};
          vector<int>ans;
 
-         int n=rowIndex+1;
+        //  int n=rowIndex+1;
 
-          for(int i=1;i<=n;i++){
-            ans.push_back(ncr(rowIndex,i-1));
+        //   for(int i=1;i<=n;i++){
+        //     ans.push_back(ncr(rowIndex,i-1));
+        //   }
+
+        //   return ans;
+
+          // time complexity O(n*r)
+          // space complexity O(n) used to store the result 
+
+          // optimized approach 
+
+           long long res=1;
+          int n=rowIndex+1;  //  row number in 1 based indexing and that number of element in  a particular   array of pascal triangle 
+          ans.push_back(res);
+
+          for(int i=1;i<n;i++){
+            res=res*(n-i);
+            res/=i;
+            ans.push_back(res);
+
+
           }
 
-          return ans;
+
+           return ans;
+
+
+
 
     }
 };
