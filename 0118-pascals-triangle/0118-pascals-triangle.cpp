fd6c654af7ecd3849e4fa54d824a023cb6c8a319
harikesh tripathi <harikesht845@gmail.com>
2025-09-11 13:07:27 +0530

    Time: 0 ms (100%), Space: 9.7 MB (64.43%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -10,17 +10,52 @@ public:
     }
     return res;
    }
+
+     void getRowElement(int row,vector<int>&temp){
+
+         if(row==1){
+            temp.push_back(1);
+            return;
+         }
+         long long res=1;
+         temp.push_back(res);
+          for(int i=1;i<row;i++){
+              res=res*(row-i);
+              res=res/i;
+              temp.push_back(res);
+          }
+    
+          return;
+
+     }
+
+
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans;
-
-        for(int i=1;i<=numRows;i++){
-            vector<int>temp;
-            for(int col=1;col<=i;col++ ){
-                temp.push_back(ncr(i-1,col-1));
-            }
-            ans.push_back(temp);
-        }
         
-        return ans;
+        // for(int i=1;i<=numRows;i++){
+        //     vector<int>temp;
+        //     for(int col=1;col<=i;col++ ){
+        //         temp.push_back(ncr(i-1,col-1));
+        //     }
+        //     ans.push_back(temp);
+        // }
+        
+        // return ans;
+
+        // time complexity O(n^2*r) where n=number of row and r is number of column in worst case O(n^3) if n=r
+
+
+        // optimized approach it will takes the time O(N^2)
+       
+         for(int i=1;i<=numRows;i++){
+            vector<int>temp;
+           getRowElement(i,temp);
+           ans.push_back(temp);
+
+         }
+
+         return ans;
+
     }
 };
