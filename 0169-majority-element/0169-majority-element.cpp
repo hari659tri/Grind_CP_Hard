b52f87c47019784282ad21dbabbe8e9a6749f858
harikesh tripathi <harikesht845@gmail.com>
2025-09-15 14:20:12 +0530

    Time: 0 ms (100%), Space: 28.4 MB (7.36%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        unordered_map<int,int>mp;
+         for(auto it:nums){
+          mp[it]++;
+          if(mp[it]>n/2) return it;
+
+         }
+
+         return -1;
+    }
+};
