45c8559ea0a82b82f94dec97bbef3544954646cd
harikesh tripathi <harikesht845@gmail.com>
2025-09-19 11:28:58 +0530

    Time: 4 ms (68.85%), Space: 23.8 MB (57.53%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -10,14 +10,14 @@ public:
         vector<vector<int>>ans;
         sort(intervals.begin(),intervals.end(),cmp);
        vector<int>temp=intervals[0];
-
-        for(auto it:intervals){
-            if(it[0]<=temp[1]){
-                temp[1]=max(temp[1],it[1]);
+        int n=intervals.size();
+        for(auto it=1;it<n;it++){
+            if(intervals[it][0]<=temp[1]){
+                temp[1]=max(temp[1],intervals[it][1]);
             }
             else{
                 ans.push_back(temp);
-                temp=it;
+                temp=intervals[it];
             }
         }
         
