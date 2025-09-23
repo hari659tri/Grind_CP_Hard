06117f0d7b1f7e98349a9f6d9e82cc758324d5a9
harikesh tripathi <harikesht845@gmail.com>
2025-09-23 11:44:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.53%) - LeetHub

--- 1279-prime-arrangements/1279-prime-arrangements.cpp
+++ 1279-prime-arrangements/1279-prime-arrangements.cpp
@@ -26,16 +26,36 @@ public:
      int numPrimeArrangements(int n) {
            
            long long  primecount=0;
-         for(int i=1;i<=n;i++){
-            if(prime(i)){
-                primecount++;
+        //  for(int i=1;i<=n;i++){
+        //     if(prime(i)){+
+        //         primecount++;
+        //     }
+        //  }
+
+        //  seve algo 
+        vector<bool>p(n+1,1);
+        p[0]=p[1]=0;
+         for(int i=2;i<=sqrt(n);i++){
+            if(p[i]==1){
+                for(int j=i*i;j<=n;j+=i) p[j]=0;
             }
          }
-
+          
+          for(auto it:p){
+            if(it) primecount++;
+          }
          long long  nonp=n-primecount;
       
          long long ans=(fact(primecount)*fact(nonp))%mod;
 
          return ans%mod;
+
+
+         // time complexity is O(nroot(n))
+         /// also if impement sive it go to in O(nloglogn) time complexity....
+
+
+         // be can also boli down to it O(nloglogn) using a sieve algorithm....
+         
     }
 };
