bba6da87b4bd05a1739dedc1cdbc75c7e7427e37
harikesh tripathi <harikesht845@gmail.com>
2025-09-27 13:19:08 +0530

    Time: 0 ms (100%), Space: 13.4 MB (35.93%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        for (int i = triangle.size() - 2; i >= 0; i--)
-            for (int j = 0; j < triangle[i].size(); j++)
-                triangle[i][j] +=
-                    min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+    
+    int solve(vector<vector<int>>& triangle,int row,int col,int m, vector<vector<int>>&dp){
 
-        return triangle[0][0];
+        if(row==m){
+            return dp[row][col]=triangle[row][col];
+        }
+        if(dp[row][col]!=-1e5) return dp[row][col];
+
+       int  ans=triangle[row][col]+min(solve(triangle,row+1,col,m,dp),solve(triangle,row+1,col+1,m,dp));
+       return dp[row][col]=ans;
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        // we are go to memoized this solution to it number of changing parameters 2 show 2 d array will required 
+        int row=0;
+        int col=0;
+        int n= triangle.size();
+        
+        // number of row =maximum number of column are prsented 
+        //so...
+        vector<vector<int>>dp(n,vector<int>(n,-1e5));
+       
+        int ans= solve(triangle,row,col,n-1,dp);
+
+        return ans;
     }
 };
