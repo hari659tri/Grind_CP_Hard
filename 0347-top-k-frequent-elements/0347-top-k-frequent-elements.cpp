01f586bbca4ecb5642bd08bc7ebe46b30bb4f580
harikesh tripathi <harikesht845@gmail.com>
2025-11-10 16:12:18 +0530

    Time: 0 ms (100%), Space: 18 MB (37.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -9,25 +9,46 @@ public:
         
         // brute force approach
 
-        unordered_map<int,int>mp;
+        unordered_map<int,int>mp;//O(N)
         int n=nums.size();
 
          for(int i=0;i<n;i++){
             mp[nums[i]]++;
          }
+        // O(N)
+    //      vector<pair<int,int>>vec;
+    //      for(auto it:mp){
+    //         vec.push_back({it.first,it.second});
+    //      } ///O(N)
 
-         vector<pair<int,int>>vec;
-         for(auto it:mp){
-            vec.push_back({it.first,it.second});
-         }
+    //      sort(vec.begin(),vec.end(),cmp); //O(nlogn)
 
-         sort(vec.begin(),vec.end(),cmp);
+    //      vector<int>ans;
+    //      for(int i=0;i<k;i++){
+    //         ans.push_back(vec[i].first);
+    //      }
+      
+    //    return ans;
+
+       // time complexity O(NlogN)
+       //space O(N)
+
+
+       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+            for(auto it:mp){
+           pq.push({it.second,it.first});
+           if(pq.size()>k) pq.pop();
+         } 
 
          vector<int>ans;
-         for(int i=0;i<k;i++){
-            ans.push_back(vec[i].first);
+
+         while(!pq.empty()){
+           int a= pq.top().second;
+          ans.push_back(a);
+          pq.pop();
          }
-      
-       return ans;
+
+         return ans;
+         
     }
 };
