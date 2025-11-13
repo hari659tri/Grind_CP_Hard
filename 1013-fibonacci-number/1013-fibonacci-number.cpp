91d05c61dbbe8e837ad5f397ebad94b70562d40f
harikesh tripathi <harikesht845@gmail.com>
2025-11-13 11:11:30 +0530

    Time: 14 ms (22.03%), Space: 7.7 MB (86.92%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-   
-     int f(int n){
-        
-        //base case here we are write
-        if(n==0||n==1) return n;
-
-
-        return f(n-1)+f(n-2);
-     }
     int fib(int n) {
-          
-        return f(n);
+
+        if(n==1||n==0) return n;
+
+        return fib(n-1)+fib(n-2);
     }
 };
