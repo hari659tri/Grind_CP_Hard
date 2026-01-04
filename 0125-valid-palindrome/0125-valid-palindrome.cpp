30e1cf1049904f886be5d111bba4b897fae7d386
harikesh tripathi <harikesht845@gmail.com>
2026-01-04 17:54:39 +0530

    Time: 1 ms (46.46%), Space: 9.9 MB (55.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,68 +1,49 @@
 class Solution {
-public: 
+public:
+  
+  bool isalpha(char ch){
 
-  bool alpha(char ch1){
-    if(('0'<=ch1&&ch1<='9')||('a'<=ch1&&ch1<='z')||('A'<=ch1&&ch1<='Z')) return true;
-    // if(('0'<=ch2&&ch2<='9')||('a'<=ch2&&ch2<='z')) return true;
+    if(97<=tolower(ch)&&tolower(ch)<=122)
+       return true;
 
-    return false;
+       if('0'<=ch&&ch<='9') 
+         return true;
 
+        
+         return false;
 
   }
-
-
-    bool check(int i,int j,string &s){
-        
-         if(i>=j) return true;
-         
-        if(!isalnum(s[i])){
-           return check(i+1,j,s);
-        }
-        if(!isalnum(s[j])){
-            return check(i,j-1,s);
-        }
-
-      if(tolower(s[i])!=tolower(s[j])) return false;
-     
-        
-      return  check(i+1,j-1,s);
-
-     
-    }
     bool isPalindrome(string s) {
         
-        // i can solve it recursion first right it's iterative code
-
-        int n=s.size();
         int i=0;
+        int n=s.size();
         int j=n-1;
 
-        // recursive implementation 
+         if(!isalpha(s[i])) 
+           i++;
 
-       return check(i,j,s);
+         if(!isalpha(s[j]))
+           j--;
+
+           while(i<j){
+            if(isalpha(s[i])&&isalpha(s[j])){
+                if(tolower(s[i])==tolower(s[j])){
+                    i++;
+                    j--;
+                }
+                else
+                return false;
+            }
+            
+            if(!isalpha(s[i])) 
+           i++;
+    
+            if(!isalpha(s[j]))
+           j--;
+
+           }
 
 
-        //  while(i<j){
-           
-        //     if(!alpha(s[i])){
-        //      i++;
-        //       continue;
-        //     } 
-
-        //      if(!alpha(s[j])){
-        //      j--;
-        //       continue;
-        //     } 
-
-
-        //      if(tolower(s[i])!=tolower(s[j])){
-        //         return false;
-        //      }
-        //      i++;
-        //      j--;
-        //  }
-
-
-        //  return true;
+     return true;
     }
 };
