8ca19735d8ad2dec639fecbac74381f7b4994173
harikesh tripathi <harikesht845@gmail.com>
2026-01-15 14:10:44 +0530

    Time: 3 ms (59.88%), Space: 16.2 MB (9.51%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+      vector<string>ans;
+     
+    
+     void solve(string s,int n,int idx, string temp){
+        if(idx==n){
+            ans.push_back(temp);
+            return;
+        }
+       if(idx>=n) return;
+        if(isdigit(s[idx])){
+            temp+=s[idx];
+            solve(s,n,idx+1,temp);
+        }
+        
+        if(isalpha(s[idx])){
+
+            temp+=toupper(s[idx]); 
+            solve(s,n,idx+1,temp);
+            temp.pop_back();
+            temp+=tolower(s[idx]);
+            solve(s,n,idx+1,temp);
+            temp.pop_back();
+           
+        }
+        
+          
+
+     }
+    vector<string> letterCasePermutation(string s) {
+        int n=s.size();
+         string temp;
+        solve(s,n,0,temp);
+
+        return ans;
+
+    }
+};
