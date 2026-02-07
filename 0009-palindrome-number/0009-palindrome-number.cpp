231471167ccedaf182c0b63dd3c2901ad7d81758
harikesh tripathi <harikesht845@gmail.com>
2026-02-07 22:50:01 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
-        if(x<0) 
-        return false;
-
-        int org=x;
-        long long res=0;
-        while(x>0){
-
-            int r=x%10;
-            res=res*10+r;
+        if(0<=x&&x<=9) return true;
+        if(x<0) return false;
+         int org=x;
+          long long  rev=0;
+         while(x>0){
+            long long rem=x%10;
+            rev=rev*10+rem;
             x=x/10;
-        }
-      
+         }
 
-      if(org==res) return true;
+         if(rev==org) 
+           return true;
 
-      return false;
+
+           return false;
 
     }
 };
