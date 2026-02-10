6329ae35cd71e598c5fee0111702aadd4c047cdd
harikesh tripathi <harikesht845@gmail.com>
2026-02-10 14:59:49 +0530

    Time: 0 ms (100%), Space: 13.7 MB (7.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,74 +1,33 @@
 class Solution {
 public:
-   
-   bool bs(vector<int>&v,int target){
-    int i=0;
-    int j=v.size()-1;
-
-    while(i<=j){
-        int mid=i+(j-i)/2;
-
-        if(v[mid]==target) return true;
-
-        else if(v[mid]<target) i=mid+1;
-        else j=mid-1;
-        
-    }
-    return false;
-   }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        vector<int>flat;
         int m=matrix.size();
         int n=matrix[0].size();
 
-      
-    //   for(int i=0;i<m;i++){
-    //     for(int j=0;j<n;j++){
-    //         if(matrix[i][j]==target) return true;
-    //     }
-    //   }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                flat.push_back(matrix[i][j]);
+            }
+        }
 
-      //return false;
-    //   bool ans=false;
-    //   for(int i=0;i<m;i++){
+        int l=0;
+        int h=m*n-1;
 
-    //     if(matrix[i][0]<=target&&target<=matrix[i][n-1]){
-    //        ans= bs(matrix[i],target);
-    //     }
-    //   }
+        while(l<=h){
+            int mid=l+(h-l)/2;
 
-     
-       //if(ans) return true;
-     
-    // return ans==true?true: false;
+            if(flat[mid]==target){
+                return true;
+            }
+            else if(flat[mid]>target){
+                h=mid-1;
+                
+            }
+            else
+                l=mid+1;
+        }
 
-     // time complexity O(mlogn) where m=no of row 
-     // n=no of coloumn...
-     //space complexity O(1)
-
-     // optimized approach....
-
-     // flatten a 2-D matrix into an a 1-D matrix in mind(Brain) not in really
-
-      // now 
-      int low=0;
-      int high=m*n-1;
-
-      while(low<=high){
-        int mid=low+(high-low)/2;
-       
-       int row=mid/n;
-       int col=mid%n;
-
-       if(matrix[row][col]==target) return true;
-       else if(matrix[row][col]<target) low=mid+1;
-       else  high=mid-1;
-
-      }
-
-      return false;
-      
-
-      
+     return false;
     }
 };
