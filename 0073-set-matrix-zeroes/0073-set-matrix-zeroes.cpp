9f9a1b094ff261f5114074a154b7f602631cfd07
harikesh tripathi <harikesht845@gmail.com>
2026-02-20 10:55:59 +0530

    Time: 1 ms (14.98%), Space: 19.1 MB (5.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<bool> zeroRows(rows, false);
-        vector<bool> zeroCols(cols, false);
+        
+        vector<vector<int>>temp=matrix;
 
-        // First pass: mark rows and columns that need to be zeroed
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
+        int r=matrix.size();
+        int c=matrix[0].size();
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==0){
+                    for(int k=0;k<c;k++){
+                        temp[i][k]=0;
+                    }
+
+                    for(int k=0;k<r;k++){
+                        temp[k][j]=0;
+                    }
                 }
             }
         }
 
-        // Second pass: zero out marked rows
-        for (int i = 0; i < rows; ++i) {
-            if (zeroRows[i]) {
-                for (int j = 0; j < cols; ++j) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Second pass: zero out marked columns
-        for (int j = 0; j < cols; ++j) {
-            if (zeroCols[j]) {
-                for (int i = 0; i < rows; ++i) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+        matrix=temp;
     }
 };
