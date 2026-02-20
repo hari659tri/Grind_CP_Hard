c07d0ba3cdc8fde90830cd5c955cee356eabbbd7
harikesh tripathi <harikesht845@gmail.com>
2026-02-20 13:42:32 +0530

    Time: 39 ms (72.94%), Space: 45.4 MB (54.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,27 +2,36 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         
-        unordered_map<int,int>mp; // {sum,frequency } k 
-        mp[0]=1;  // edge case important 
+        // int cnt=0;
+        // int n=nums.size();
 
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         int sum=0;
+        //         for(int m=i;m<=j;m++){
+        //             sum+=nums[m];
+        //         }
+        //         if(sum==k) 
+        //         cnt++;
+        //     }
+        // }
+
+        // return cnt;
+
+        unordered_map<int,int>mp; //{sum,count}
+        mp[0]=1;  
+        int n=nums.size();
+        int cumSum=0;
         int ans=0;
 
-        // running sum
-        int  n=nums.size();
-        int pre=0;
         for(int i=0;i<n;i++){
-            pre=pre+nums[i];
-          
-          if(mp.find(pre-k)!=mp.end()){
-             ans+=mp[pre-k];
-          }
+           cumSum+=nums[i];
+           if(mp.find(cumSum-k)!=mp.end()){
+            ans+=mp[cumSum-k];
+           }  
+           mp[cumSum]++;
+        }
 
-            mp[pre]++;
-
-        } 
-  
-       
-       return ans;
-
+        return ans;
     }
 };
