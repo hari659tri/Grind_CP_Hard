4eaf411f42d909749f699d651a804314d7004daa
harikesh tripathi <harikesht845@gmail.com>
2026-02-20 11:01:22 +0530

    Time: 3 ms (10.63%), Space: 18.6 MB (79.06%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,25 +2,60 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        vector<vector<int>>temp=matrix;
+       // vector<vector<int>>temp=matrix;
 
         int r=matrix.size();
         int c=matrix[0].size();
 
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         if(matrix[i][j]==0){
+        //             for(int k=0;k<c;k++){
+        //                 temp[i][k]=0;
+        //             }
+
+        //             for(int k=0;k<r;k++){
+        //                 temp[k][j]=0;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // matrix=temp;
+
+
+        //this is an a brute force appraoch 
+        // for the optimal approach we use to sotre the row number as a boolean 
+
+
+        vector<bool>row(r,false);
+        vector<bool>col(c,false);
+
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<c;k++){
-                        temp[i][k]=0;
-                    }
-
-                    for(int k=0;k<r;k++){
-                        temp[k][j]=0;
-                    }
+                    row[i]=true;
+                    col[j]=true;
                 }
             }
         }
 
-        matrix=temp;
+        for(int i=0;i<r;i++){
+            if(row[i]){
+                for(int j=0;j<c;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+   
+      for(int j=0;j<c;j++){
+            if(col[j]){
+                for(int i=0;i<r;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+
     }
 };
