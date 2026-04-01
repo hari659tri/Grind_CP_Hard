5ffe0fdaaf161c07b15b21e1ec3f5d1720a278bb
harikesh tripathi <harikesht845@gmail.com>
2026-04-01 10:51:11 +0530

    Time: 0 ms (100%), Space: 21.7 MB (75.22%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>>ans;
 
-       int n=intervals.size();
-       int i=0;
-       while(i<n&&intervals[i][1]<newInterval[0]){
-        ans.push_back(intervals[i]);
-        i++;
-       }
+         vector<vector<int>>ans;
+         int n=intervals.size();
+         int i=0;
 
-       while(i<n&&intervals[i][0]<=newInterval[1]){
-        newInterval[0]=min(intervals[i][0],newInterval[0]);
-        newInterval[1]=max(intervals[i][1],newInterval[1]);
-        i+=1;
-       }
-     
-        ans.push_back(newInterval);
-
-        while(i<n&&intervals[i][0]>newInterval[1]){
+         // this  question is solved in simple 3 steps...
+         //step1:add all the intervals before the new intervals
+         //step2:Merge the overlapping intervals 
+         //step3:add Remaning Intervlas 
+         
+         while(i<n&&intervals[i][1]<newInterval[0]){
             ans.push_back(intervals[i]);
-            i+=1;
-        }
+            i++;
+         }
 
+         //merge the overlapping intervals...
 
-      return ans;
+         while(i<n&&intervals[i][0]<=newInterval[1]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            i++;
 
+         }
+         ans.push_back(newInterval);
 
+         while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
+
+         }
+        
+         return ans;
     }
 };
