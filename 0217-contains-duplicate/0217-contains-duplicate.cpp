31eb4b5dce53bd538a7130d832451dce6a8d9646
harikesh tripathi <harikesht845@gmail.com>
2026-06-01 19:41:45 +0530

    Time: 44 ms (72.51%), Space: 90.7 MB (61.89%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    
+        int n=nums.size();
+        // use hashing for it 
+        unordered_set<int>st;
+        for(auto it:nums){
+
+          if(st.find(it)!=st.end()){
+            return true;
+          }
+            st.insert(it);
+        }
+     
+        return false;
+        
+
+    }
+};
